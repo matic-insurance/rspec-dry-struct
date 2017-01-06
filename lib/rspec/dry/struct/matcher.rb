914a25d7b9dcc7eb82d9a25d5c28b9daf98c57e2
Sergey Kukunin <sergey.kukunin@gmail.com>
2017-01-06 15:23:06 +0200

    Support attribute matcher without type

--- lib/rspec/dry/struct/matcher.rb
+++ lib/rspec/dry/struct/matcher.rb
@@ -4,7 +4,7 @@ module RSpec
       class Matcher
         attr_reader :attr_name, :attr_type
 
-        def initialize(attr_name, attr_type)
+        def initialize(attr_name, attr_type = nil)
           @attr_name = attr_name
           @attr_type = attr_type
         end
@@ -12,7 +12,7 @@ module RSpec
         def matches?(actual)
           @actual = actual
           @actual_attribute = actual.schema[@attr_name]
-          @actual_attribute == @attr_type
+          attr_present? && correct_attr_type?
         end
 
         def description
@@ -25,6 +25,14 @@ module RSpec
         end
 
         private
+
+        def attr_present?
+          !@actual_attribute.nil?
+        end
+
+        def correct_attr_type?
+          @attr_type.nil? || @actual_attribute == @attr_type
+        end
 
         def attr_not_found_message(actual)
           "#{expected_message(actual)}, but it was not found"
--- spec/rspec/dry/struct/matcher_spec.rb
+++ spec/rspec/dry/struct/matcher_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe RSpec::Dry::Struct::Matcher do
   subject { matcher }
 
   let(:attr_name) { :first_name }
-  let(:attr_type) { Types::Strict::String }
 
   let(:struct_class) do
     Class.new(Dry::Types::Struct) do
@@ -14,7 +13,7 @@ RSpec.describe RSpec::Dry::Struct::Matcher do
     end
   end
 
-  let(:matcher) { described_class.new(attr_name, attr_type) }
+  let(:matcher) { described_class.new(attr_name) }
 
   context 'with matching attr_name and type' do
     it 'matches' do
@@ -36,17 +35,28 @@ RSpec.describe RSpec::Dry::Struct::Matcher do
     end
   end
 
-  context 'with wrong type' do
-    let(:attr_type) { Types::String }
+  context 'with specific type' do
+    let(:attr_type) { Types::Strict::String }
+    let(:matcher) { described_class.new(attr_name, attr_type) }
 
-    it 'fails' do
-      expect(matcher.matches?(struct_class)).to eq false
+    context 'with matching attr_name and type' do
+      it 'matches' do
+        expect(matcher.matches?(struct_class)).to eq true
+      end
     end
 
-    it 'contains failure message' do
-      matcher.matches?(struct_class)
-      expect(matcher.failure_message)
-        .to eq "expected #{struct_class} to have :first_name attribute, but type is wrong"
+    context 'with wrong type' do
+      let(:attr_type) { Types::String }
+
+      it 'fails' do
+        expect(matcher.matches?(struct_class)).to eq false
+      end
+
+      it 'contains failure message' do
+        matcher.matches?(struct_class)
+        expect(matcher.failure_message)
+          .to eq "expected #{struct_class} to have :first_name attribute, but type is wrong"
+      end
     end
   end
 
